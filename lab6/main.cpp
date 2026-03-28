94cc79e41f107f424cda7785945caaf2ff250255
Dan259 <156999793+Dan259@users.noreply.github.com>
2026-03-28 22:59:50 +1000

    cvxcvxvc

--- lab6/main.cpp
+++ lab6/main.cpp
@@ -9,14 +9,16 @@ int byear[4], byear1;
 
 int main(){
 	while(fgets(str,100,data1) != NULL){
-		printf("%s",str);
-		for(int i = (strlen(str) - 5),j = 0; j < 4; i++, j++){
+		//printf("%s",str);
+		for(int i = (strlen(str) - 5),j = 0; j < 5; i++, j++){
 			byear[j] = str[i] - '0';
-			printf("\n%d %c\n",byear[j],str[i]);
+			byear[j] = byear[j] > 0 ? byear[j] : 0;
 		}
-		//printf("\n%d%d%d%d",byear[0],byear[1],byear[2],byear[3]);
-		byear1 = byear[0] * 1000 + byear[1] * 100 + byear[2] * 10 + byear[3];
-		printf("\n%d",byear1);
+		byear1 = byear[0] == 0 ? byear[1] * 1000 + byear[2] * 100 + byear[3] * 10 + byear[4] : byear[0] * 1000 + byear[1] * 100 + byear[2] * 10 + byear[3];
+		if(byear1 > 1980){
+			fputs(str,data2);
+		}
+		printf("%d\n", byear1);
 	}
 	
 }
