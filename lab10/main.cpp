bf7f990ce2ef990c6bae5616f6b1a51e3bce7475
Dan259 <156999793+Dan259@users.noreply.github.com>
2026-05-23 00:07:20 +1000

    Lab10

--- lab10/main.cpp
+++ lab10/main.cpp
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAX(x,y) x > y ? 1:0
+#define MAX(arr,cnt) do { \
+	int max = 0; \
+	for(int j = 0; j < cnt;j++){ \
+		if(arr[j] > max) \
+			max = arr[j]; \
+	} \
+	printf("%d",max); \
+} while(0)
+
+
 
 int main(){
+	char row1[100];
+	int row[10];
+	printf("Enter numbers row:");
+	scanf("%[^\n]",&row1);
 	
+	char *token = strtok(row1," ");
+	int i = 0;
+	
+	while(token != NULL){
+		row[i] = atoi(token);
+		token = strtok(NULL," ");
+		i++;
+	}
+	
+	MAX(row,i);
 }
